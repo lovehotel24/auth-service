a841ef0c0660d913a154312f1809d2bd5a4b5cb6
D Ther Htun <dtherhtun.cw@gmail.com>
2024-03-16 18:16:53 +0700

    fix: security fix update user

--- pkg/controller/user.go
+++ pkg/controller/user.go
@@ -62,194 +62,205 @@ func getBookUserById(userId string, grpcClient userpb.UserServiceClient) (User, 
 	return user, true
 }
 
-func (a API) CurrentUser() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		userId, ok := c.Get(userKey)
-		if !ok {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to current user"})
-			return
-		}
-		user, done := getBookUserById(userId.(string), a.Grpc)
-		if !done {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get current user"})
-			return
-		}
-		c.JSON(http.StatusOK, &user)
+func (a API) CurrentUser(c *gin.Context) {
+	userId, ok := c.Get(userKey)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to current user"})
+		return
 	}
+	user, done := getBookUserById(userId.(string), a.Grpc)
+	if !done {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get current user"})
+		return
+	}
+	c.JSON(http.StatusOK, &user)
 }
 
-func (a API) GetUser() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		userId := c.Param("id")
-		user, done := getBookUserById(userId, a.Grpc)
-		if !done {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get user"})
-			return
-		}
-		//configs.DB.Where("id = ?").First(&user)
-		c.JSON(http.StatusOK, &user)
+func (a API) GetUser(c *gin.Context) {
+
+	userId := c.Param("id")
+	user, done := getBookUserById(userId, a.Grpc)
+	if !done {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get user"})
+		return
 	}
+
+	c.JSON(http.StatusOK, &user)
 }
 
-func (a API) GetUsers() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		//var users []User
-		//blank := &empty.Empty{}
-		//allUsers, err := grpcClient.GetAllUsers(context.Background(), blank)
-		//if err != nil {
-		//	return
-		//}
-		//for _, v := range allUsers.GetUsers() {
-		//	user := User{
-		//		Id:    v.GetId().GetValue(),
-		//		Name:  v.GetName(),
-		//		Phone: v.GetPhone(),
-		//		Role:  v.GetRole(),
-		//	}
-		//	users = append(users, user)
-		//}
-		//c.JSON(http.StatusOK, &users)
-		limit := 10
-		offset, _ := strconv.Atoi(c.Query("offset"))
+func (a API) GetUsers(c *gin.Context) {
+	limit := 10
+	offset, _ := strconv.Atoi(c.Query("offset"))
 
-		req := &userpb.GetAllUserRequest{
-			Limit:  int32(limit),
-			Offset: int32(offset),
-		}
-
-		allUsers, err := a.Grpc.GetAllUsers(context.Background(), req)
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch users"})
-			return
-		}
-
-		var users []User
-		for _, v := range allUsers.GetUsers() {
-			user := User{
-				Id:    v.GetId().GetValue(),
-				Name:  v.GetName(),
-				Phone: v.GetPhone(),
-				Role:  v.GetRole(),
-			}
-			users = append(users, user)
-		}
-
-		c.JSON(http.StatusOK, &users)
+	req := &userpb.GetAllUserRequest{
+		Limit:  int32(limit),
+		Offset: int32(offset),
 	}
+
+	allUsers, err := a.Grpc.GetAllUsers(context.Background(), req)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch users"})
+		return
+	}
+
+	var users []User
+	for _, v := range allUsers.GetUsers() {
+		user := User{
+			Id:    v.GetId().GetValue(),
+			Name:  v.GetName(),
+			Phone: v.GetPhone(),
+			Role:  v.GetRole(),
+		}
+		users = append(users, user)
+	}
+
+	c.JSON(http.StatusOK, &users)
 }
 
-func (a API) CreateUser() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var user User
-		var dbUser models.DBUser
-		if err := c.BindJSON(&user); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-
-		userId := NewUserId()
-		dbUser.Id = userId
-
-		dbUser.Phone = user.Phone
-
-		if hash, ok := generateHashPasswd(c, user.Password); ok {
-			dbUser.PasswordHash = hash
-		} else {
-			return
-		}
-		user.Password = ""
-
-		_, err := a.Grpc.CreateUser(context.Background(), &userpb.CreateUserRequest{User: &userpb.User{
-			Id:    &userpb.UUID{Value: userId.String()},
-			Name:  user.Name,
-			Phone: user.Phone,
-			Role:  user.Role,
-		}})
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
-			return
-		}
-		a.DB.Create(&dbUser)
-		c.JSON(http.StatusOK, &user)
+func (a API) CreateUser(c *gin.Context) {
+	var user User
+	var dbUser models.DBUser
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
+
+	userId := NewUserId()
+	dbUser.Id = userId
+	user.Id = userId.String()
+
+	dbUser.Phone = user.Phone
+
+	if hash, ok := generateHashPasswd(c, user.Password); ok {
+		dbUser.PasswordHash = hash
+	} else {
+		return
+	}
+	user.Password = ""
+
+	_, err := a.Grpc.CreateUser(context.Background(), &userpb.CreateUserRequest{User: &userpb.User{
+		Id:    &userpb.UUID{Value: userId.String()},
+		Name:  user.Name,
+		Phone: user.Phone,
+		Role:  user.Role,
+	}})
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+		return
+	}
+	a.DB.Create(&dbUser)
+	c.JSON(http.StatusOK, &user)
+
 }
 
-func (a API) UpdateUser() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var updateUser User
-		var dbUpdateUser models.DBUser
-		userId, ok := c.Get(userKey)
-		if !ok {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
+func (a API) UpdateUser(c *gin.Context) {
+	var (
+		updateUser     User
+		saveToDBUser   models.DBUser
+		updateBookUser *userpb.User
+	)
+
+	userId, ok := c.Get(userKey)
+	if !ok {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to save session"})
+		return
+	}
+
+	if err := c.BindJSON(&updateUser); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	if c.Param("id") == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "empty param"})
+		return
+	}
+
+	updateUser.Id = c.Param("id")
+	id, err := uuid.Parse(c.Param("id"))
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "invalid user id"})
+		return
+	}
+	saveToDBUser.Id = id
+	updateBookUser = &userpb.User{Id: &userpb.UUID{Value: c.Param("id")}}
+
+	currentUser, _ := getBookUserById(userId.(string), a.Grpc)
+	bookUser, _ := getBookUserById(c.Param("id"), a.Grpc)
+	dbUser := a.getDBUserById(c.Param("id"))
+
+	switch currentUser.Role {
+	case "ADMIN", "USER":
+		if currentUser.Role == "USER" && c.Param("id") != userId.(string) {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "You are not authorized to update other users' information"})
 			return
-		}
-		user := a.getDBUserById(userId)
-		if err := c.BindJSON(&updateUser); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-			return
-		}
-		//configs.DB.Model(models.DBUser{}).Where("id = ?", user.Id).First(&currentUser)
-		if updateUser.Id != userId.(string) {
-			c.JSON(http.StatusBadRequest, gin.H{"error": "invalid user ID for the update operation"})
-			return
-		}
-		updateBookUser := &userpb.User{
-			Id: &userpb.UUID{Value: userId.(string)},
 		}
 
 		if updateUser.Phone != "" {
-			dbUpdateUser.Phone = updateUser.Phone
+			saveToDBUser.Phone = updateUser.Phone
 			updateBookUser.Phone = updateUser.Phone
 		} else {
-			dbUpdateUser.Phone = user.Phone
+			saveToDBUser.Phone = dbUser.Phone
+			updateUser.Phone = dbUser.Phone
 		}
 
 		if updateUser.Password != "" {
 			if hash, ok := generateHashPasswd(c, updateUser.Password); ok {
-				dbUpdateUser.PasswordHash = hash
+				saveToDBUser.PasswordHash = hash
 			} else {
 				return
 			}
 		} else {
-			dbUpdateUser.PasswordHash = user.PasswordHash
+			saveToDBUser.PasswordHash = dbUser.PasswordHash
 		}
 
 		if updateUser.Name != "" {
 			updateBookUser.Name = updateUser.Name
+		} else {
+			updateUser.Name = bookUser.Name
 		}
 
 		if updateUser.Role != "" {
 			updateBookUser.Role = updateUser.Role
+		} else {
+			updateUser.Role = bookUser.Role
 		}
 
-		_, err := a.Grpc.UpdateUser(context.Background(), &userpb.UpdateUserRequest{User: updateBookUser})
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to update user"})
-			return
-		}
-
-		updateUser.Password = ""
-
-		if err := a.DB.Save(dbUpdateUser).Error; err != nil {
-			fmt.Println(err)
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error})
-			return
-		}
-		c.JSON(http.StatusOK, &user)
+	default:
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Unauthorized user role for this operation"})
+		return
 	}
+
+	if _, err = a.Grpc.UpdateUser(context.Background(), &userpb.UpdateUserRequest{User: updateBookUser}); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to perform user update"})
+		return
+	}
+
+	updateUser.Password = ""
+
+	if err := a.DB.Save(saveToDBUser).Error; err != nil {
+		fmt.Println(err)
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error})
+		return
+	}
+
+	c.JSON(http.StatusOK, &updateUser)
 }
 
-func (a API) DeleteUser() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var user models.DBUser
-		_, err := a.Grpc.DeleteUser(context.Background(), &userpb.DeleteUserRequest{Id: &userpb.UUID{Value: c.Param("id")}})
-		if err != nil {
-			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete user"})
-			return
-		}
-		a.DB.Where("id = ?", c.Param("id")).Delete(&user)
-		c.JSON(http.StatusOK, &user)
+func (a API) DeleteUser(c *gin.Context) {
+	var user models.DBUser
+	if _, err := a.Grpc.DeleteUser(context.Background(), &userpb.DeleteUserRequest{Id: &userpb.UUID{Value: c.Param("id")}}); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete the user"})
+		return
 	}
+
+	if err := a.DB.Where("id = ?", c.Param("id")).Delete(&user).Error; err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to delete the user"})
+		return
+	}
+
+	c.JSON(http.StatusOK, &user)
+
 }
 
 type forgetPass struct {
@@ -267,7 +278,11 @@ func (a API) ForgetPass(c *gin.Context) {
 		VerifyCode: configs.EncodeToString(6),
 		UserId:     user.Id,
 	}
-	a.DB.Create(&newReset)
+	if err := a.DB.Create(&newReset).Error; err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
 	c.JSON(http.StatusOK, &newReset)
 }
 
@@ -285,7 +300,7 @@ func (a API) ResetPass(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	fmt.Println(reset.ConfirmPassword)
+
 	if reset.Password != reset.ConfirmPassword {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "password and confirm password must be identical."})
 		return
@@ -302,10 +317,6 @@ func (a API) ResetPass(c *gin.Context) {
 	a.DB.Save(&user)
 	a.DB.Model(&models.ResetPass{}).Delete(&forget)
 	c.JSON(http.StatusOK, &user)
-}
-
-func Hello(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{"hello": "world"})
 }
 
 func generateHashPasswd(c *gin.Context, pass string) ([]byte, bool) {
